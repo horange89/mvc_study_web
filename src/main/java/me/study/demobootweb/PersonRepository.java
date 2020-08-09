f13a7098af25293f2f806f3b9cd9d4a6fc664939
horange89 <sbo78@naver.com>
2020-08-09 21:42:26 +0900

    * MVC Study 2

---
+++ src/main/java/me/study/demobootweb/PersonRepository.java
@@ -0,0 +1,7 @@
+package me.study.demobootweb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
---
+++ src/main/java/me/study/demobootweb/SampleController.java
@@ -0,0 +1,14 @@
+package me.study.demobootweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String hello(@PathVariable("id") Person person) {
+        return "hello " + person.getName();
+    }
+}
