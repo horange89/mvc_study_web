736cd8ec5e7e17a3c50b5f35b7029ab9f0889a2e
horange89 <sbo78@naver.com>
2020-08-11 20:08:53 +0900

    * 핸들러 인터셉터 구현

--- src/main/java/me/study/demobootweb/SampleController.java
+++ src/main/java/me/study/demobootweb/SampleController.java
@@ -2,13 +2,23 @@ package me.study.demobootweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
+    // preHandle 1
+    // preHandle 2
+    // 요청처리
+    // postHandle 2
+    // postHandle 1
+    // 뷰 랜더링
+    // afterCompletiton 2
+    // afterCompletiton 1
+
     @GetMapping("/hello")
-    public String hello(@PathVariable("id") Person person) {
+    public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
     }
 }
--- src/main/java/me/study/demobootweb/WebConfig.java
+++ src/main/java/me/study/demobootweb/WebConfig.java
@@ -1,9 +1,17 @@
 package me.study.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
 }
